66de43ea97e33a2c368c70aa5f0b5a358177c727
AVPDesign <60422075+AVPDesign@users.noreply.github.com>
2022-05-15 00:15:27 +0200

    Empty components made, dummy data set up

---
+++ eventyrfrolerne/src/components/FrolComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h3>{{ frol.name }}</h3>
+        <p>{{ frol.info }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "FrolComponent",
+    props: {
+        frol: Object,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- eventyrfrolerne/src/sections/FrolSection.vue
+++ eventyrfrolerne/src/sections/FrolSection.vue
@@ -2,18 +2,59 @@
     <section>
         <NavTo section="Frol"/>
         <HeadingTwo heading="Frol"/>
+        <FrolComponent 
+            v-for="frol in frols" 
+            :key="frol.id" 
+            :frol="frol" 
+        />
     </section>
 </template>
 
 <script>
 import NavTo from "@/components/NavTo.vue"
 import HeadingTwo from "@/components/HeadingTwo.vue"
+import FrolComponent from "@/components/FrolComponent.vue"
 
 export default {
     name: "FrolSection",
     components: {
         NavTo,
         HeadingTwo,
+        FrolComponent,
+    },
+    data() {
+        return {
+            frols: [],
+        };
+    },
+    created() {
+        this.frols = [
+            {
+                id: 1,
+                name: "Rødbede",
+                info: "Lorem ipsum dolor sit amet...",
+            },
+            {
+                id: 2,
+                name: "Gulerod",
+                info: "Lorem ipsum dolor sit amet...",
+            },
+            {
+                id: 3,
+                name: "Ranunkel",
+                info: "Lorem ipsum dolor sit amet...",
+            },
+            {
+                id: 4,
+                name: "Kartoffel",
+                info: "Lorem ipsum dolor sit amet...",
+            },
+            {
+                id: 1,
+                name: "Radisse",
+                info: "Lorem ipsum dolor sit amet...",
+            },
+        ]
     }
 }
 </script>
